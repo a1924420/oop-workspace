3dc8d18b090bb38cc9f21526d8de3560d60ab1ae
Mikayla Brierley <mikaylabrierley@Mikaylas-MacBook-Pro-3.local>
2024-08-06 17:46:58 +0930

    ws3

--- main.cpp
+++ main.cpp
@@ -1,11 +1,56 @@
 #include <iostream>
 
-// The `extern` keyword tells the compiler that the count function exists,
-// but will be implemented somewhere else
-extern int count(int[], int);
+extern void changeValue(double* ptr);
 
-int main() {
-    int array[5] = {4,5,6,7,8};
-    std::cout << "The number is: " << count(array, 5) << std::endl;
+extern void printArray(double* array, int size);
+
+extern double arrayMax(double* array, int size);
+
+extern double* dynamicArray(int N, double M);
+
+int main(){
+
+    double a = 12;
+
+    std::cout << a << std::endl;
+
+    changeValue(&a);
+
+    std::cout << a << std::endl;
+
     return 0;
+
+    double array[] = {2.4,3.5,4.6,5.7,6.8,7.9};
+
+    int size = sizeof(array) / sizeof(array[0]);
+
+    printArray(array, size);
+
+    return 0;
+
+    double array[] = {6.5,3.4,2.2,9.1,6.8};
+
+    int size = sizeof(array) / sizeof(array[0]);
+
+    arrayMax(array,size);
+
+    std::cout << arrayMax(array,size) << std::endl;
+
+    return 0;
+
+    int N = 9;
+
+    double M = 9;
+
+    double* array = dynamicArray(N, M);
+
+    for (int i = 0; i < N; i++){
+        std::cout << array[i] << " ";
+    }
+    std::cout<< std::endl;
+
+    delete[] array;
+
+    return 0;
+
 }
