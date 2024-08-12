4e4062f4fbb063834b550415446b060914701e17
Mikayla Brierley <mikaylabrierley@Mikaylas-MacBook-Pro-3.local>
2024-08-12 19:22:03 +0930

    ws4

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
-int readNumbers(){
+int *readNumbers(){
 
-    
+    int *numbers = new int[10];
+    int number;
+
+    for (int i = 0; i < 10; i++){
+        std::cout << "Enter a number: ";
+        std::cin >> number;
+        numbers[i] = number;
+    }
+
+    return numbers;
+
+}
+
+void printNumbers(int *numbers, int length){
+
+    for (int i = 0; i < length; i++){
+        std::cout << i << " " << numbers[i] << std::endl;
+    }
 
 }
