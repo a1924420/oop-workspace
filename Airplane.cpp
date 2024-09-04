04e394956ace6f902b002ded4b5dc4ae54a10331
Mikayla Brierley <mikaylabrierley@Mikaylas-MacBook-Pro-3.local>
2024-09-04 16:15:29 +0930

    pe3

--- Airplane.cpp
+++ Airplane.cpp
@@ -4,10 +4,25 @@
 
 Airplane::Airplane(){}
 
-Airplane::Airplane(int w, int p){}
+Airplane::Airplane(int w, int p){
+    weight = w;
+    numPassengers = p;
+}
 
-void Airplane::reducePassengers(int x){}
+void Airplane::reducePassengers(int x){
+    numPassengers -= x;
+}
 
-int Airplane::get_numPassengers(){}
+int Airplane::get_numPassengers(){ return numPassengers;}
 
-void Airplane::fly(int headwind, int minutes){}
+void Airplane::fly(int headwind, int minutes){
+    if (headwind < 60){
+        fuel = fuel - (0.3 * minutes) - (0.001 * numPassengers * minutes);
+    } else if (headwind >= 60){
+        fuel = fuel - (0.5 * minutes) - (0.001 * numPassengers * minutes);
+    }
+
+    if (fuel >= 20){
+        numberOfFlights++;
+    } 
+}
--- main-2-2.cpp
+++ main-2-2.cpp
@@ -4,5 +4,27 @@
 
 int main(){
 
+    Airplane b1;
+
+    std::cout << "Weight: " << b1.get_weight() << std::endl;
+    std::cout << "Fuel: " << b1.get_fuel() << std::endl;
+    std::cout << "Number of Flights: " << b1.get_numberOfFlights() << std::endl;
+    std::cout << "numPassengers: " << b1.get_numPassengers() << std::endl;
+
+
+    Airplane b2(5000, 200);
+
+    std::cout << "Weight: " << b2.get_weight() << std::endl;
+    std::cout << "Fuel: " << b2.get_fuel() << std::endl;
+    std::cout << "Number of Flights: " << b2.get_numberOfFlights() << std::endl;
+    std::cout << "numPassengers: " << b2.get_numPassengers() << std::endl;
+
+    b2.fly(20, 2);
+
+    std::cout << "Weight: " << b2.get_weight() << std::endl;
+    std::cout << "Fuel: " << b2.get_fuel() << std::endl;
+    std::cout << "Number of Flights: " << b2.get_numberOfFlights() << std::endl;
+    std::cout << "numPassengers: " << b2.get_numPassengers() << std::endl;
+
     return 0;
 }
