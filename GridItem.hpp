5d654a575249ae1854b79e2921c4baa375472907
Mikayla Brierley <mikaylabrierley@Mikaylas-MacBook-Pro-3.local>
2024-10-23 15:28:39 +1030

    p

--- GridItem.hpp
+++ GridItem.hpp
@@ -1,4 +1,42 @@
 #ifndef GRIDITEM_H
 #define GRIDITEM_H
 
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+class GridItem{
+
+    private:
+
+    std::pair<int, int> coordinates;
+
+    int width;
+
+    int height;
+
+    int activeGridItemCount;
+
+    public:
+
+    GridItem(int x, int y, int width, int height) : coordinates(x, y), width(width), height(height){}
+
+    GridItem() : coordinates(0, 0), width(0), height(0){}
+
+    void setCoordinates(int x, int y){
+        this->coordinates = std::make_pair(x, y);
+    }
+
+    std::pair<int, int> getCoordinates(){ return coordinates; }
+
+    int getGridWidth(){ return width; }
+
+    int getGridHeight(){ return height; }
+
+    int getActiveGridItemCount(){ return activeGridItemCount; }
+
+    virtual ~GridItem() = 0;
+
+};
+
 #endif
--- Helper.hpp
+++ Helper.hpp
@@ -1,4 +1,25 @@
 #ifndef HELPER_H
 #define HELPER_H
 
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <cmath>
+
+class Helper{
+
+    private:
+
+    public:
+
+    int manhattanDistance(std::pair<int, int> item1, std::pair<int, int> item2){
+
+        int distance = (abs(item2.first - item1.first) + abs(item2.second = item2.second));
+
+        return distance;
+
+    }
+
+};
+
 #endif
